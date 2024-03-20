8c30cf9dc673e1210901577b7423cc541731fdb7
zenith <stazenith@outlook.com>
2024-03-19 22:09:58 -0700

    updated setting.py

--- app/conf/development/settings.py
+++ app/conf/development/settings.py
@@ -66,8 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_FILE_PATH = join(CONTENT_DIR, 'tmp/emails')
 EMAIL_HOST_USER = 'test@example.com'
